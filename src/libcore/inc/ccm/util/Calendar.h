0684e5179c472cb0b4458fabbce42527c2850b63
jingcao80 <jingcao80@gmail.com>
2018-09-06 21:02:49 +0800

    continued on Calendar.

--- src/libcore/inc/ccm/util/Calendar.h
+++ src/libcore/inc/ccm/util/Calendar.h
@@ -22,6 +22,7 @@
 #include "ccm.core.IComparable.h"
 #include "ccm.io.ISerializable.h"
 #include "ccm.util.ICalendar.h"
+#include "ccm.util.IDate.h"
 #include "ccm.util.ILocale.h"
 #include "ccm.util.ITimeZone.h"
 
@@ -48,11 +49,105 @@ protected:
 
     ECode Constructor(
         /* [in] */ ITimeZone* zone,
-        /* [in] */ ILocale* alocale);
+        /* [in] */ ILocale* locale);
+
+    static AutoPtr<ICalendar> GetInstance();
+
+    static AutoPtr<ICalendar> GetInstance(
+        /* [in] */ ITimeZone* zone);
+
+    static AutoPtr<ICalendar> GetInstance(
+        /* [in] */ ILocale* aLocale);
+
+    static AutoPtr<ICalendar> GetInstance(
+        /* [in] */ ITimeZone* zone,
+        /* [in] */ ILocale* aLocale);
+
+    static AutoPtr<ICalendar> GetJapaneseImperialInstance(
+        /* [in] */ ITimeZone* zone,
+        /* [in] */ ILocale* aLocale);
+
+    static Array<ILocale*> GetAvailableLocales();
+
+    ECode GetTime(
+        /* [out] */ IDate** date) override final;
+
+    ECode SetTime(
+        /* [in] */ IDate* date) override final;
+
+    ECode GetTimeInMillis(
+        /* [out] */ Long* time) override;
+
+    ECode SetTimeInMillis(
+        /* [in] */ Long millis) override;
+
+    ECode Get(
+        /* [in] */ Integer field,
+        /* [out] */ Integer* value) override;
+
+    ECode Set(
+        /* [in] */ Integer field,
+        /* [in] */ Integer value) override;
+
+    ECode Set(
+        /* [in] */ Integer year,
+        /* [in] */ Integer month,
+        /* [in] */ Integer date) override final;
+
+    ECode Set(
+        /* [in] */ Integer year,
+        /* [in] */ Integer month,
+        /* [in] */ Integer date,
+        /* [in] */ Integer hourOfDay,
+        /* [in] */ Integer minute) override final;
+
+    ECode Set(
+        /* [in] */ Integer year,
+        /* [in] */ Integer month,
+        /* [in] */ Integer date,
+        /* [in] */ Integer hourOfDay,
+        /* [in] */ Integer minute,
+        /* [in] */ Integer second) override final;
+
+    ECode Clear() override final;
+
+    ECode Clear(
+        /* [in] */ Integer field) override final;
+
+    ECode IsSet(
+        /* [in] */ Integer field,
+        /* [out] */ Boolean* result) override final;
+
+protected:
+    virtual ECode ComputeTime() = 0;
+
+    virtual ECode ComputeFields() = 0;
+
+    Integer InternalGet(
+        /* [in] */ Integer field);
+
+    void InternalSet(
+        /* [in] */ Integer field,
+        /* [in] */ Integer value);
+
+    virtual ECode Complete()
+    {
+        return NOERROR;
+    }
 
 private:
+    static AutoPtr<ICalendar> CreateCalendar(
+        /* [in] */ ITimeZone* zone,
+        /* [in] */ ILocale* aLocale);
+
     void SetWeekCountData(
         /* [in] */ ILocale* desiredLocale)
+    {}
+
+    void UpdateTime()
+    {}
+
+    void AdjustStamp()
     {}
 
 protected:
@@ -60,13 +155,40 @@ protected:
 
     Array<Boolean> mIsSet;
 
+    Long mTime = 0;
+
+    Boolean mIsTimeSet = false;
+
+    Boolean mAreFieldsSet = false;
+
+    Boolean mAreAllFieldsSet = false;
+
 private:
     Array<Integer> mStamp;
 
     AutoPtr<ITimeZone> mZone;
 
     Boolean mSharedZone = false;
+
+    static constexpr Integer UNSET = 0;
+
+    static constexpr Integer MINIMUM_USER_STAMP = 2;
+
+    Integer mNextStamp = MINIMUM_USER_STAMP;
 };
+
+inline Integer Calendar::InternalGet(
+    /* [in] */ Integer field)
+{
+    return mFields[field];
+}
+
+inline void Calendar::InternalSet(
+    /* [in] */ Integer field,
+    /* [in] */ Integer value)
+{
+    mFields[field] = value;
+}
 
 }
 }
--- src/libcore/src/ccm/util/Calendar.cpp
+++ src/libcore/src/ccm/util/Calendar.cpp
@@ -16,11 +16,14 @@
 
 #include "ccm/util/Calendar.h"
 #include "ccm/util/CLocale.h"
+#include "ccm/util/CDate.h"
 #include "ccm/util/TimeZone.h"
+#include "ccm.core.IInteger.h"
 #include <ccmautoptr.h>
 
 using ccm::core::IID_ICloneable;
 using ccm::core::IID_IComparable;
+using ccm::core::IInteger;
 using ccm::io::IID_ISerializable;
 
 namespace ccm {
@@ -50,6 +53,216 @@ ECode Calendar::Constructor(
 
     mZone = zone;
     SetWeekCountData(aLocale);
+    return NOERROR;
+}
+
+AutoPtr<ICalendar> Calendar::GetInstance()
+{
+    return CreateCalendar(TimeZone::GetDefault(),
+            CLocale::GetDefault(CLocale::Category::GetFORMAT()));
+}
+
+AutoPtr<ICalendar> Calendar::GetInstance(
+    /* [in] */ ITimeZone* zone)
+{
+    return CreateCalendar(zone,
+            CLocale::GetDefault(CLocale::Category::GetFORMAT()));
+}
+
+AutoPtr<ICalendar> Calendar::GetInstance(
+    /* [in] */ ILocale* aLocale)
+{
+    return CreateCalendar(TimeZone::GetDefault(), aLocale);
+}
+
+AutoPtr<ICalendar> Calendar::GetInstance(
+    /* [in] */ ITimeZone* zone,
+    /* [in] */ ILocale* aLocale)
+{
+    return CreateCalendar(zone, aLocale);
+}
+
+AutoPtr<ICalendar> Calendar::GetJapaneseImperialInstance(
+    /* [in] */ ITimeZone* zone,
+    /* [in] */ ILocale* aLocale)
+{
+    return nullptr;
+}
+
+AutoPtr<ICalendar> Calendar::CreateCalendar(
+    /* [in] */ ITimeZone* zone,
+    /* [in] */ ILocale* aLocale)
+{
+    return nullptr;
+}
+
+Array<ILocale*> Calendar::GetAvailableLocales()
+{
+    return Array<ILocale*>::Null();
+}
+
+ECode Calendar::GetTime(
+    /* [out] */ IDate** date)
+{
+    VALIDATE_NOT_NULL(date);
+
+    Long time;
+    GetTimeInMillis(&time);
+    return CDate::New(time, IID_IDate, (IInterface**)date);
+}
+
+ECode Calendar::SetTime(
+    /* [in] */ IDate* date)
+{
+    Long time;
+    date->GetTime(&time);
+    return SetTimeInMillis(time);
+}
+
+ECode Calendar::GetTimeInMillis(
+    /* [out] */ Long* time)
+{
+    VALIDATE_NOT_NULL(time);
+
+    if (!mIsTimeSet) {
+        UpdateTime();
+    }
+    *time = mTime;
+    return NOERROR;
+}
+
+ECode Calendar::SetTimeInMillis(
+    /* [in] */ Long millis)
+{
+    if (mTime = millis && mIsTimeSet && mAreFieldsSet && mAreAllFieldsSet) {
+        return NOERROR;
+    }
+    mTime = millis;
+    mIsTimeSet = true;
+    mAreFieldsSet = false;
+    ComputeFields();
+    mAreAllFieldsSet = mAreFieldsSet = true;
+    return NOERROR;
+}
+
+ECode Calendar::Get(
+    /* [in] */ Integer field,
+    /* [out] */ Integer* value)
+{
+    VALIDATE_NOT_NULL(value);
+
+    if (field < 0 || field >= FIELD_COUNT) {
+        return ccm::core::E_ARRAY_INDEX_OUT_OF_BOUNDS_EXCEPTION;
+    }
+
+    Complete();
+    *value = InternalGet(field);
+    return NOERROR;
+}
+
+ECode Calendar::Set(
+    /* [in] */ Integer field,
+    /* [in] */ Integer value)
+{
+    if (field < 0 || field >= FIELD_COUNT) {
+        return ccm::core::E_ARRAY_INDEX_OUT_OF_BOUNDS_EXCEPTION;
+    }
+
+    if (mAreFieldsSet && ! mAreAllFieldsSet) {
+        ComputeFields();
+    }
+    InternalSet(field, value);
+    mIsTimeSet = false;
+    mAreFieldsSet = false;
+    mIsSet[field] = true;
+    mStamp[field] = mNextStamp++;
+    if (mNextStamp == IInteger::MAX_VALUE) {
+        AdjustStamp();
+    }
+    return NOERROR;
+}
+
+ECode Calendar::Set(
+    /* [in] */ Integer year,
+    /* [in] */ Integer month,
+    /* [in] */ Integer date)
+{
+    Set(YEAR, year);
+    Set(MONTH, month);
+    Set(DATE, date);
+    return NOERROR;
+}
+
+ECode Calendar::Set(
+    /* [in] */ Integer year,
+    /* [in] */ Integer month,
+    /* [in] */ Integer date,
+    /* [in] */ Integer hourOfDay,
+    /* [in] */ Integer minute)
+{
+    Set(YEAR, year);
+    Set(MONTH, month);
+    Set(DATE, date);
+    Set(HOUR_OF_DAY, hourOfDay);
+    Set(MINUTE, minute);
+    return NOERROR;
+}
+
+ECode Calendar::Set(
+    /* [in] */ Integer year,
+    /* [in] */ Integer month,
+    /* [in] */ Integer date,
+    /* [in] */ Integer hourOfDay,
+    /* [in] */ Integer minute,
+    /* [in] */ Integer second)
+{
+    Set(YEAR, year);
+    Set(MONTH, month);
+    Set(DATE, date);
+    Set(HOUR_OF_DAY, hourOfDay);
+    Set(MINUTE, minute);
+    Set(SECOND, second);
+    return NOERROR;
+}
+
+ECode Calendar::Clear()
+{
+    for (Integer i = 0; i < mFields.GetLength(); ) {
+        mStamp[i] = mFields[i] = 0;
+        mIsSet[i++] = false;
+    }
+    mAreAllFieldsSet = mAreFieldsSet = false;
+    mIsTimeSet = false;
+    return NOERROR;
+}
+
+ECode Calendar::Clear(
+    /* [in] */ Integer field)
+{
+    if (field < 0 || field >= FIELD_COUNT) {
+        return ccm::core::E_ARRAY_INDEX_OUT_OF_BOUNDS_EXCEPTION;
+    }
+
+    mFields[field] = 0;
+    mStamp[field] = UNSET;
+    mIsSet[field] = false;
+
+    mAreAllFieldsSet = mAreFieldsSet = false;
+    mIsTimeSet = false;
+    return NOERROR;
+}
+
+ECode Calendar::IsSet(
+    /* [in] */ Integer field,
+    /* [out] */ Boolean* result)
+{
+    VALIDATE_NOT_NULL(result);
+
+    if (field < 0 || field >= FIELD_COUNT) {
+        return ccm::core::E_ARRAY_INDEX_OUT_OF_BOUNDS_EXCEPTION;
+    }
+
+    *result = mStamp[field] != UNSET;
     return NOERROR;
 }
 
