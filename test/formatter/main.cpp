408ff2a8a6f7b9db6aadbd22dd542b0c7d1d3739
jingcao80 <jingcao80@gmail.com>
2019-03-30 21:56:59 +0800

    dadd Formatter testcases.

--- test/formatter/main.cpp
+++ test/formatter/main.cpp
@@ -16,17 +16,36 @@
 
 #include "core/CoreUtils.h"
 #include "core/StringUtils.h"
+#include "ccm.util.CCalendarFactory.h"
 #include "ccm.util.CLocale.h"
+#include "ccm.util.CLocaleFactory.h"
+#include "ccm.util.CTimeZoneFactory.h"
+#include "ccm.util.ICalendar.h"
+#include "ccm.util.ICalendarFactory.h"
 #include "ccm.util.ILocale.h"
+#include "ccm.util.ILocaleFactory.h"
+#include "ccm.util.ITimeZone.h"
+#include "ccm.util.ITimeZoneFactory.h"
 #include <ccmautoptr.h>
 #include <gtest/gtest.h>
 
 using namespace ccm;
 using ccm::core::CoreUtils;
 using ccm::core::StringUtils;
+using ccm::util::CCalendarFactory;
 using ccm::util::CLocale;
+using ccm::util::CLocaleFactory;
+using ccm::util::CTimeZoneFactory;
+using ccm::util::IID_ICalendarFactory;
 using ccm::util::IID_ILocale;
+using ccm::util::IID_ILocaleFactory;
+using ccm::util::IID_ITimeZoneFactory;
+using ccm::util::ICalendar;
+using ccm::util::ICalendarFactory;
 using ccm::util::ILocale;
+using ccm::util::ILocaleFactory;
+using ccm::util::ITimeZone;
+using ccm::util::ITimeZoneFactory;
 
 TEST(FormatterTest, TestNumberLocalization)
 {
@@ -41,6 +60,245 @@ TEST(FormatterTest, TestNumberLocalization)
         U' ', U'3', U'4'
     };
     EXPECT_STREQ(arabicStr.string(), String(arabicChars).string());
+
+    arabicStr = StringUtils::Format(arabic, String("12 %,d 34"), &args);
+    arabicChars = {
+        U'1', U'2', U' ',
+        0x0661, 0x066c, 0x0662, 0x0663, 0x0664, 0x066c, 0x0665, 0x0666, 0x0667, 0x066c, 0x0668, 0x0669, 0x0660,
+        U' ', U'3', U'4'
+    };
+    EXPECT_STREQ(arabicStr.string(), String(arabicChars).string());
+
+    args = { CoreUtils::Box(1.23) };
+    arabicStr = StringUtils::Format(arabic, String("12 %.3e 34"), &args);
+    arabicChars = {
+        U'1', U'2', U' ',
+        0x0661, 0x066b, 0x0662, 0x0663, 0x0660, 0x0627, 0x0633, U'+', 0x0660, 0x0660,
+        U' ', U'3', U'4'
+    };
+    EXPECT_STREQ(arabicStr.string(), String(arabicChars).string());
+
+    arabicStr = StringUtils::Format(arabic, String("12 %.3f 34"), &args);
+    arabicChars = {
+        U'1', U'2', U' ',
+        0x0661, 0x066b, 0x0662, 0x0663, 0x0660,
+        U' ', U'3', U'4'
+    };
+    EXPECT_STREQ(arabicStr.string(), String(arabicChars).string());
+
+    arabicStr = StringUtils::Format(arabic, String("12 %.3g 34"), &args);
+    arabicChars = {
+        U'1', U'2', U' ',
+        0x0661, 0x066b, 0x0662, 0x0663,
+        U' ', U'3', U'4'
+    };
+    EXPECT_STREQ(arabicStr.string(), String(arabicChars).string());
+    // And date/time formatting (we assume that all time/date number formatting is done by the
+    // same code, so this is representative):
+    // AutoPtr<ITimeZoneFactory> tzFactory;
+    // CTimeZoneFactory::New(IID_ITimeZoneFactory, (IInterface**)&tzFactory);
+    // AutoPtr<ITimeZone> tz;
+    // tzFactory->GetTimeZone(String("GMT-08:00"), &tz);
+    // AutoPtr<ICalendarFactory> calFactory;
+    // CCalendarFactory::New(IID_ICalendarFactory, (IInterface**)&calFactory);
+    // AutoPtr<ICalendar> c;
+    // calFactory->GetInstance(tz, &c);
+    // c->SetTimeInMillis(0);
+    // args = { c };
+    // arabicStr = StringUtils::Format(arabic, String("12 %tT 34"), &args);
+    // arabicChars = {
+    //     U'1', U'2', U' ',
+    //     0x0661, 0x0666, U':', 0x0660, 0x0660, U':', 0x0660, 0x0660,
+    //     U' ', U'3', U'4'
+    // };
+    // EXPECT_STREQ(arabicStr.string(), String(arabicChars).string());
+    // These shouldn't get localized:
+    arabicStr = StringUtils::Format(arabic, String("1234"), nullptr);
+    EXPECT_STREQ(arabicStr.string(), "1234");
+    args = { CoreUtils::Box(String("1234")) };
+    arabicStr = StringUtils::Format(arabic, String("%s"), &args);
+    EXPECT_STREQ(arabicStr.string(), "1234");
+    args = { CoreUtils::Box(1234) };
+    arabicStr = StringUtils::Format(arabic, String("%s"), &args);
+    EXPECT_STREQ(arabicStr.string(), "1234");
+    args = { CoreUtils::Box(1234) };
+    arabicStr = StringUtils::Format(arabic, String("%o"), &args);
+    EXPECT_STREQ(arabicStr.string(), "2322");
+    args = { CoreUtils::Box(1234) };
+    arabicStr = StringUtils::Format(arabic, String("%x"), &args);
+    EXPECT_STREQ(arabicStr.string(), "4d2");
+    args = { CoreUtils::Box(1.0) };
+    arabicStr = StringUtils::Format(arabic, String("%a"), &args);
+    EXPECT_STREQ(arabicStr.string(), "0x1.0p0");
+}
+
+TEST(FormatterTest, TestInternationalizedExponent)
+{
+    AutoPtr<ILocaleFactory> factory;
+    CLocaleFactory::New(IID_ILocaleFactory, (IInterface**)&factory);
+    AutoPtr<ILocale> eng;
+    factory->GetENGLISH(&eng);
+
+    Array<IInterface*> args{ CoreUtils::Box(100.0) };
+    String engStr = StringUtils::Format(eng, String("%.0E"), &args);
+    EXPECT_STREQ(engStr.string(), "1E+02");
+
+    engStr = StringUtils::Format(eng, String("%.0e"), &args);
+    EXPECT_STREQ(engStr.string(), "1e+02");
+
+    AutoPtr<ILocale> arabic;
+    CLocale::New(String("ar"), IID_ILocale, (IInterface**)&arabic);
+
+    String arabicStr = StringUtils::Format(arabic, String("%.0E"), &args);
+    Array<Char> arabicChars{
+        0x0661, 0x0627, 0x0633, U'+', 0x0660, 0x0662
+    };
+    EXPECT_STREQ(arabicStr.string(), String(arabicChars).string());
+
+    arabicStr = StringUtils::Format(arabic, String("%.0e"), &args);
+    arabicChars = {
+        0x0661, 0x0627, 0x0633, U'+', 0x0660, 0x0662
+    };
+    EXPECT_STREQ(arabicStr.string(), String(arabicChars).string());
+
+    AutoPtr<ILocale> et;
+    CLocale::New(String("et"), IID_ILocale, (IInterface**)&et);
+
+    String etStr = StringUtils::Format(et, String("%.0E"), &args);
+    Array<Char> etChars = {
+        U'1', 0x00d7, U'1', U'0', U'^', U'+', U'0', U'2'
+    };
+    EXPECT_STREQ(etStr.string(), String(etChars).string());
+
+    etStr = StringUtils::Format(et, String("%.0e"), &args);
+    etChars = {
+        U'1', 0x00d7, U'1', U'0', U'^', U'+', U'0', U'2'
+    };
+    EXPECT_STREQ(etStr.string(), String(etChars).string());
+}
+
+TEST(FormatterTest, TestUppercaseConversiont)
+{
+    AutoPtr<ILocaleFactory> factory;
+    CLocaleFactory::New(IID_ILocaleFactory, (IInterface**)&factory);
+    AutoPtr<ILocale> us;
+    factory->GetENGLISH(&us);
+
+    Array<IInterface*> args{ CoreUtils::Box(String("jakob arjouni")) };
+    String usStr = StringUtils::Format(us, String("%S"), &args);
+    EXPECT_STREQ(usStr.string(), "JAKOB ARJOUNI");
+
+#ifdef __aarch64__
+    AutoPtr<ILocale> tr;
+    CLocale::New(String("tr"), String("TR"), IID_ILocale, (IInterface**)&tr);
+    String trStr = StringUtils::Format(tr, String("%S"), &args);
+    Array<Char> trChars{ 0x0130 };
+    EXPECT_STREQ(trStr.string(), (String("JAKOB ARJOUN") + String(trChars)).string());
+#endif
+}
+
+TEST(FormatterTest, TestNumberFormatReuse)
+{
+    Array<IInterface*> args{ CoreUtils::Box(7.0), CoreUtils::Box(7) };
+    String usStr = StringUtils::Format(String("%.6f %d"), &args);
+    EXPECT_STREQ(usStr.string(), "7.000000 7");
+}
+
+TEST(FormatterTest, TestGrouping)
+{
+    // The interesting case is -123, where you might naively output "-,123" if you're just
+    // inserting a separator every three characters. The cases where there are three digits
+    // before the first separator may also be interesting.
+    Array<IInterface*> args{ CoreUtils::Box(-1) };
+    String usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "-1");
+
+    args = { CoreUtils::Box(-12) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "-12");
+
+    args = { CoreUtils::Box(-123) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "-123");
+
+    args = { CoreUtils::Box(-1234) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "-1,234");
+
+    args = { CoreUtils::Box(-12345) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "-12,345");
+
+    args = { CoreUtils::Box(-123456) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "-123,456");
+
+    args = { CoreUtils::Box(-1234567) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "-1,234,567");
+
+    args = { CoreUtils::Box(-12345678) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "-12,345,678");
+
+    args = { CoreUtils::Box(-123456789) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "-123,456,789");
+
+    args = { CoreUtils::Box(1) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "1");
+
+    args = { CoreUtils::Box(12) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "12");
+
+    args = { CoreUtils::Box(123) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "123");
+
+    args = { CoreUtils::Box(1234) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "1,234");
+
+    args = { CoreUtils::Box(12345) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "12,345");
+
+    args = { CoreUtils::Box(123456) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "123,456");
+
+    args = { CoreUtils::Box(1234567) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "1,234,567");
+
+    args = { CoreUtils::Box(12345678) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "12,345,678");
+
+    args = { CoreUtils::Box(123456789) };
+    usStr = StringUtils::Format(String("%,d"), &args);
+    EXPECT_STREQ(usStr.string(), "123,456,789");
+}
+
+TEST(FormatterTest, TestFormatNull)
+{
+    // We fast-path %s and %d (with no configuration) but need to make sure we handle the
+    // special case of the null argument...
+    AutoPtr<ILocaleFactory> factory;
+    CLocaleFactory::New(IID_ILocaleFactory, (IInterface**)&factory);
+    AutoPtr<ILocale> us;
+    factory->GetENGLISH(&us);
+
+    String usStr = StringUtils::Format(us, String("%s"), nullptr);
+    EXPECT_STREQ(usStr.string(), "null");
+
+    usStr = StringUtils::Format(us, String("%d"), nullptr);
+    EXPECT_STREQ(usStr.string(), "null");
+
+    usStr = StringUtils::Format(us, String("%%"), nullptr);
+    EXPECT_STREQ(usStr.string(), "%");
 }
 
 int main(int argc, char **argv)
